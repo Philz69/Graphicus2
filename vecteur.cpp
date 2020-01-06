09077aeaa499f41e5e9801f3b9ca36104c20e5ea
Philipe Goulet <phil.gou69@hotmail.com>
2020-01-06 17:59:47 -0500

    Added empty functions for vecteur class

--- vecteur.cpp
+++ vecteur.cpp
@@ -7,4 +7,49 @@ Vecteur::Vecteur()
     capacity = 1; 
 }
 
+int Vecteur::getCapacity()
+{
+    return capacity;
+}
 
+int Vecteur::getSize()
+{
+    return size;
+}
+
+int Vecteur::isEmpty()
+{
+    if(size == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool Vecteur::add(int *forme)
+{
+    return true;
+}
+
+int* Vecteur::del(int index)
+{
+    return NULL;
+}
+
+int* Vecteur::get(int index)
+{
+    return NULL;
+}
+
+void Vecteur::empty()
+{
+    return;
+}
+
+void Vecteur::output()
+{
+    return;
+}
